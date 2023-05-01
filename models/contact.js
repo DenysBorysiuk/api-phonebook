bb75689773acfdd86fa6454947499188181d4735
Denys Borysiuk <denisborisiuk@gmail.com>
2023-05-02 00:23:18 +0300

    fix contacts model

--- models/contact.js
+++ models/contact.js
@@ -1,14 +1,11 @@
-const Joi = require("joi");
-const { Schema, model } = require("mongoose");
+const Joi = require('joi');
+const { Schema, model } = require('mongoose');
 
 const contactSchema = new Schema(
   {
     name: {
       type: String,
-      required: [true, "Set name for contact"],
-    },
-    email: {
-      type: String,
+      required: [true, 'Set name for contact'],
     },
     phone: {
       type: String,
@@ -19,7 +16,7 @@ const contactSchema = new Schema(
     },
     owner: {
       type: Schema.Types.ObjectId,
-      ref: "user",
+      ref: 'user',
     },
   },
   { versionKey: false, timestamps: true }
@@ -27,13 +24,11 @@ const contactSchema = new Schema(
 
 const addSchema = Joi.object({
   name: Joi.string().min(3).required(),
-  email: Joi.string().email().required(),
   phone: Joi.string().min(6).required(),
 });
 
 const updateSchema = Joi.object({
   name: Joi.string().min(3).optional(),
-  email: Joi.string().email().optional(),
   phone: Joi.string().min(6).optional(),
 })
   .required()
@@ -43,6 +38,6 @@ const updateFavoriteSchema = Joi.object({
   favorite: Joi.boolean().required(),
 });
 
-const Contact = model("contact", contactSchema);
+const Contact = model('contact', contactSchema);
 
 module.exports = { addSchema, updateSchema, updateFavoriteSchema, Contact };
